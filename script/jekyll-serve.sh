81d7f801bd387269e46c78201635849fa1c284ea
Robert Tucker <robertwtucker@gmail.com>
2016-02-01 23:00:15 -0500

    Jekyll now watches by default

--- script/jekyll-serve.sh
+++ script/jekyll-serve.sh
@@ -9,4 +9,4 @@ if [[ "$LIMIT_POSTS" -gt 0 ]]; then
   echo -e "\033[1;33m[WARN] Telling jekyll to only generate the last $LIMIT_POSTS posts. To change this limit, set the LIMIT_POSTS environment variable.\033[0m"
 fi
 
-bundle exec jekyll serve --watch "$SHOW_DRAFTS" --limit_posts "$LIMIT_POSTS" --force_polling -H 0.0.0.0 -P 4000
+bundle exec jekyll serve "$SHOW_DRAFTS" --limit_posts "$LIMIT_POSTS" --force_polling -H 0.0.0.0 -P 4000
